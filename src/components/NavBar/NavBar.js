b0a9b80c573dbb5314183834a90ea7c42f085929
Ana Paula Cortes <anapaulacortesibarra@gmail.com>
2021-11-04 17:10:13 -0300

    segundo desafío

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,31 @@
+import './NavBar.css';
+import logo from './NavBar.img/logo.png'
+
+
+
+
+export default function  NavBar() {
+
+    return (
+
+        <div className="header">
+            <img class="logo" src={logo} alt="logo" width="5%"/>
+
+
+            <nav>
+                <ul>
+                    <li>Productos</li>
+                    <li>Sobre nosotras</li>
+                    <li>Contacto</li>
+                </ul>
+
+
+            </nav>
+
+        </div>
+
+
+
+    )
+
+}
